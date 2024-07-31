144f175f99b6c2c04a1c616867dec85fa69c5f93
ve4ny <maxi-maxi@bk.ru>
2024-07-31 16:23:55 +0700

    Пополнение баланса

--- resources/views/pages/refill.blade.php
+++ resources/views/pages/refill.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xl-6 col-lg-8 mx-auto">
-                    <h1 class="refill__title title">Поплнение баланса</h1>
+                    <h1 class="refill__title title">Пополнение баланса</h1>
                     <refill-form></refill-form>
                 </div>
             </div>
--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -5,17 +5,21 @@
     <div class="account">
         <div class="container">
             <div class="row">
-                <div class="col-xl-3 col-lg-4">
-                    @include('pages.components.balance')
-                    @include("pages.components.menu")
-                </div>
-                <div class="col-xl-9 col-lg-8">
-                    <div class="account-block account-orders">
-                        <div class="account-orders__title account-subtitle">Перенаправление на сервис опаты</div>
-                    </div>
+                <div class="col-xl-6 col-lg-8 mx-auto">
+                    <form class="refill-form form w-block">
+                        <div class="refill-form__item">
+                            <h2 class="refill__title title">Подождите, перенаправляем на страницу платёжного
+                                сервиса</h2>
+                        </div>
+                        <div class="refill-form__item">
+                            <p>Если перенаправление не произошло автоматически, нажмите на ссылку ниже</p>
+                        </div>
+                        <div class="refill-form__item">
+                            <a class="refill-form__btn btn" href="{{$redirectUrl}}">Перейти к оплате</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @include('footer')
