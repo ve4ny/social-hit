375b7231db2740953d9e5fc49d731ee86f3a44a2
ve4ny <maxi-maxi@bk.ru>
2024-08-09 12:05:29 +0700

    Данные ИП

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,23 +2,40 @@
     <div class="container">
         <div class="footer__content d-flex">
             <div class="footer__logo">
-                <div class="footer-logo"><img src="{{ asset('/images/general/logo.svg') }}" width="104" height="48" alt="img"/>
+                <div class="footer-logo"><img src="{{ asset('/images/general/logo.svg') }}" width="104" height="48"
+                                              alt="img"/>
                 </div>
             </div>
             @if(count($pages) > 0)
-            <div class="footer__menu">
-                <ul class="footer-menu d-flex">
-                    @foreach($pages as $title => $slug)
-                        <li class="footer-menu__item"> <a class="footer-menu__link" href="/{{$slug}}">{{$title}}</a></li>
-                    @endforeach
-                </ul>
-            </div>
+                <div class="footer__menu">
+                    <ul class="footer-menu d-flex">
+                        @foreach($pages as $title => $slug)
+                            <li class="footer-menu__item"><a class="footer-menu__link" href="/{{$slug}}">{{$title}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
             <div class="footer__contacts">
                 <ul class="footer-contacts d-flex">
-                    <li class="footer-contacts__item"> <a class="footer-contacts__link" href="tel:{{ str_replace(' ', '', optional($contacts)->phone ?? '') }}">{{ optional($contacts)->phone }}</a></li>
-                    <li class="footer-contacts__item"> <a class="footer-contacts__link" href="mailto:{{ optional($contacts)->email }}">{{ optional($contacts)->email }}</a></li>
+                    <li class="footer-contacts__item">
+                        <a class="footer-contacts__link"
+                           href="tel:{{ str_replace(' ', '', optional($contacts)->phone ?? '') }}">
+                            {{ optional($contacts)->phone }}
+                        </a>
+                    </li>
+                    <li class="footer-contacts__item">
+                        <a class="footer-contacts__link"
+                           href="mailto:{{ optional($contacts)->email }}">
+                            {{ optional($contacts)->email }}
+                        </a>
+                    </li>
                 </ul>
+                <div class="mt-2">
+                    <div>ИНН: 503825427346</div>
+                    <div class="mt-1">ОГРНИП: 324508100395019</div>
+                    <div class="mt-1">ИП Алимов Михаил Ахмедович</div>
+                </div>
             </div>
         </div>
     </div>
